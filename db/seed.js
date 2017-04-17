975cc11def1e95effcc7e0c3067f1c41d0abc2f9
Nick Lee <nicklee100@gmail.com>
2017-04-17 17:33:27 -0500

    updated seed file, not complete, not tested

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('APP/db')
-    , {User, Thing, Favorite, Promise} = db
+    , {User, Thing, Favorite, Promise, Review} = db
     , {mapValues} = require('lodash')
 
 function seedEverything() {
@@ -15,24 +15,96 @@ function seedEverything() {
   return Promise.props(seeded)
 }
 
+const tags = seed(tags,{
+  theoffice: { name: 'The Office'},
+  funny: { name: 'funny'},
+  michalscott: {michaelscott: 'Michael Scott'}
+})
+
 const users = seed(User, {
-  god: {
-    email: 'god@example.com',
-    name: 'So many names',
+  donald: {
+    name: 'Mr.Presidnet',
+    email: 'donald@trump.com',
     password: '1234',
+    salt: 'hackers',
+    address: '1600 Penn Ave',
+    admin: true,
+
   },
-  barack: {
+  obama: {
     name: 'Barack Obama',
     email: 'barack@example.gov',
-    password: '1234'
+    password: '1234',
+    salt: 'americanpie',
+    address: 'somewhere far away',
+    admin: false
   },
 })
 
-const things = seed(Thing, {
+
+
+//userBelongsToManyMeMe
+
+
+// const users = seed(User, {
+//   god: {
+//     email: 'god@example.com',
+//     name: 'So many names',
+//     password: '1234',
+//   },
+//   barack: {
+//     name: 'Barack Obama',
+//     email: 'barack@example.gov',
+//     password: '1234'
+//   },
+// })
+
+const memes = seed(meme, {
+  michael: {
+    name: 'michael',
+    price: 9.99,
+    rating: 4,
+    photo:'http://4.bp.blogspot.com/-pFRXzhUqYb0/VZNw_h4OTCI/AAAAAAAAEVQ/S94m3_LRK0A/s320/FEARED%2BOR%2BLOVED.jpg',
+    product_info:'This feature michael scott meme is from The Fight eppisode (ep 6 ) season 2 ',
+    stock: 2
+  },
+  dwight: {
+    name: 'dwight',
+    price: 7.99,
+    rating: 2,
+    photo:'http://4.bp.blogspot.com/-pFRXzhUqYb0/VZNw_h4OTCI/AAAAAAAAEVQ/S94m3_LRK0A/s320/FEARED%2BOR%2BLOVED.jpg',
+    product_info:'Dwight here is talking about stuff from his Farm... ',
+    stock: 4
+  },
+  jim: {
+    name: 'jim',
+    price: 7.99,
+    rating: 2,
+    photo:'http://s2.quickmeme.com/img/ae/ae150e79fb32592c6671db823eec58a4a7f6cd7a14847e600154ddf92e19c078.jpg',
+    product_info:'Jim product information here blah blha blah  ',
+    stock: 2
+    },
+})
+
+const things = seed(Thing, { //memes
   surfing: {name: 'surfing'},
   smiting: {name: 'smiting'},
   puppies: {name: 'puppies'},
 })
+
+
+const reviews = seed(Review, ({memes, users}) => ({
+  content: 'this here is a smashing meme',
+  title: 'superb',
+  stars: 5,
+  meme_id: memes.dwight.id,
+  user_id: users.donald.id,
+
+}))
+
+
+
+
 
 const favorites = seed(Favorite,
   // We're specifying a function here, rather than just a rows object.
@@ -136,3 +208,4 @@ function seed(Model, rows) {
 }
 
 module.exports = Object.assign(seed, {users, things, favorites})
+
