89b0cd12e23527232e12368d7e48e4536a84afb1
Mojotatan <tatan42@gmail.com>
2017-04-19 16:27:41 -0500

    style boys

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -5,25 +5,26 @@ export default (props) => {
   console.log('props', props)
   // expect to receive: array of products (which have a name, image, description, price, average rating)
   const shorten = (text) => {
-    if (text.length < 32) {
+    if (text.length < 64) {
       return text
     } else {
-      return text.slice(0, 32) + '...'
+      return text.slice(0, 64) + '...'
     }
   }
     // todo: create function for displaying ratings as stars
     // todo: refactor styling to css
   return (
     <div>
-      <ul>
+      <ul className="produce">
         {props.products.map(product => {
           return (
             <li key={product.name}>
-              <img style={{display: "inline"}} src={product.photo} alt={product.name}></img>
-              <span>
-                <div>{product.name}</div>
-                <div>{product.price} {product.rating}</div>
-                <div>{shorten(product.product_info)}</div>
+              <img src={product.photo} alt={product.name}></img>
+              <span className="in-block">
+                <div className="name">{product.name}</div>
+                <div className="price">$$$ {product.price} $$$</div>
+                <div className="rating">{product.rating}/5.00 starz based on user reviews</div>
+                <div className="info">{shorten(product.product_info)}</div>
               </span>
             </li>
           )
